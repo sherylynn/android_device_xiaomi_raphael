0f2dafff87c1bfc232881417ec55840caea0828a
UtsavBalar1231 <utsavbalar1231@gmail.com>
2020-10-16 17:56:31 +0000

    raphael: Build GoogleCamera app
    * App By Arnova8G2
    [GCam_5.2beta3.201006.2045build-7.4.104.apk]
    
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- device.mk
+++ device.mk
@@ -142,6 +142,10 @@ PRODUCT_PACKAGES += \
     libdng_sdk.vendor \
     libqti-perfd-client
 
+# GoogleCamera
+PRODUCT_PACKAGES += \
+    GoogleCamera
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.front.xml \
